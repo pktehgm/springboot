138a52621080086e7ad0940d0452da83adb8d104
Mihai Gansari <pktehgm@gmail.com>
2025-12-19 17:04:35 +0200

    first example

---
+++ src/main/java/com/mg/springboot/CourseController.java
@@ -0,0 +1,33 @@
+package com.mg.springboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+/*
+[
+    {
+        "id": 1,
+        "name": "Learn Spring Boot",
+        "author": "AAA"
+    }
+]
+*/
+
+@RestController
+public class CourseController {
+
+    // /courses - map this url
+    // Course: id, name, author
+
+    @RequestMapping("/courses")
+    public List<Course> retrieveAllCourses() {
+        return Arrays.asList(
+                new Course(1, "Learn Spring Boot", "AAA"),
+                new Course(2, "Learn Spring MVC", "BBB"),
+                new Course(3, "Learn Spring Security", "CCC")
+        );
+    }
+}
